b933d129d450a7cc6e4fa1186fe90b6cac11a0bf
kristiinlee <kristiinlee@gmail.com>
2023-11-20 08:16:24 -0500

    Updated CSS and added forecast JS

--- src/index.js
+++ src/index.js
@@ -45,7 +45,35 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function(day) {
+        forecastHtml =
+        forecastHtml + 
+        `
+        <div class="weather-forecast-day>
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temps">
+            <div class="weather-forecast-temp">
+            18°
+            </div>
+            <div class="weather-forecast-temp">
+            12°
+            </div>
+        </div>
+        `; 
+    });
+
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
